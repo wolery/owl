fa40eb392aaadbc96e4bbf87ca41a6e0b5d1efaf
Jonathon Bell <jonathon.a.bell@gmail.com>
2015-11-07 12:56:33 -0500

    Added utilities.mod.
    Implementing modes.
    
    Adding unit tests for Scale, Shape.

--- src/main/scala/com/wolery/owl/core/Utilities.scala
+++ src/main/scala/com/wolery/owl/core/Utilities.scala
@@ -44,46 +44,68 @@ object utilities
   def subscript  (s: String): String = s.map(subscript)
   def superscript(s: String): String = s.map(superscript)
 
-/**
- * Returns the non-negative remainder of the integer ''i'' on division by 12.
- *
- * The result is the unique integer 0 ≤ ''r'' < 12 such that ''i'' = 12⋅''q''
- * + ''r'' for some ''q'' in ℤ.
- *
- * @param  i an integer
- * @return the non-negative remainder of ''i'' on division by 12
- * @see    [[https://en.wikipedia.org/wiki/Modulo_operation]]
- */
-  def mod12(i: ℤ): ℕ =
+  /**
+   * Returns the non-negative remainder of the integer ''i'' upon division by
+   * the positive integer ''n''.
+   *
+   * The result is the unique integer 0 ≤ ''r'' < ''n'' such that ''i'' =
+   * ''n''⋅''q'' + ''r'' for some ''q'' in ℤ.
+   *
+   * @param  i an integer
+   * @param  n a positive integer
+   * @return the non-negative remainder of ''i'' upon division by ''n''
+   * @see    [[https://en.wikipedia.org/wiki/Modulo_operation]]
+   */
+  def mod(i: ℤ,n: ℕ): ℕ =
   {
-    val m = i % 12;
+    require(n > 0)                                       // Validate argument
 
-    if (m < 0) m + 12 else m
+    val r = i % n;                                       // Compute remainder
+
+    if (r < 0) r + n else r                              // Check non-negative
   }
 
-/**
- * Rotates the binary representation of a given integer left by the specified
- * number of bits, as if the operation were being performed within a register
- * that is 12 bits wide.
- *
- * If ''by'' is negative, the bits are instead rotated to the right.
- *
- * The result is undefined if ''bits'' occupies more than just the 12 lowest
- * bits of an integer; that is, if `bits & ~0xFFF` is non-zero.
- *
- * For example:
- * {{{
- *    rol12(0x001, 1) = 0x002
- *    rol12(0x001,23) = 0x800
- *    rol12(0x800, 1) = 0x001
- *    rol12(0x800,-4) = 0x080
- * }}}
- *
- * @param  bits a 12 bit integer
- * @param  by   the number of bit positions to rotate ''bits'' by
- * @return 			the result of rotating ''bits'' left by ''by'' bits
- * @see    [[https://en.wikipedia.org/wiki/Circular_shift]]
- */
+  /**
+   * Returns the non-negative remainder of the integer ''i'' upon division by
+   * 12.
+   *
+   * The result is the unique integer 0 ≤ ''r'' < 12 such that ''i'' = 12⋅''q''
+   * + ''r'' for some ''q'' in ℤ.
+   *
+   * @param  i an integer
+   * @return the non-negative remainder of ''i'' upon division by 12
+   * @see    [[https://en.wikipedia.org/wiki/Modulo_operation]]
+   */
+  def mod12(i: ℤ): ℕ =
+  {
+    val r = i % 12;                                      // Compute remainder
+
+    if (r < 0) r + 12 else r                             // Check non-negative
+  }
+
+  /**
+   * Rotates the binary representation of a given integer left  by a specified
+   * number of bits as if the operation were being performed within a register
+   * exactly 12 bits wide.
+   *
+   * If ''by'' is negative then the bits are instead rotated to the right.
+   *
+   * The result is undefined if ''bits'' occupies more than just the 12 lowest
+   * bits of an integer; that is, if `(bits & ~0xFFF0` is non-zero.
+   *
+   * For example:
+   * {{{
+   *    rol12(0x001, 1) = 0x002
+   *    rol12(0x001,23) = 0x800
+   *    rol12(0x800, 1) = 0x001
+   *    rol12(0x800,-4) = 0x080
+   * }}}
+   *
+   * @param  bits a 12 bit integer
+   * @param  by   the number of bit positions to rotate ''bits'' by
+   * @return 			the result of rotating ''bits'' left by ''by'' bits
+   * @see    [[https://en.wikipedia.org/wiki/Circular_shift]]
+   */
   def rol12(bits: Int,by: ℤ): Int =
   {
     require((bits & ~0xFFF) == 0,"domain error")         // Validate argument
@@ -93,29 +115,29 @@ object utilities
     0xFFF & (bits << i | bits >>> 12-i)                  // Rotate, then mask
   }
 
-/**
- * Rotates the binary representation of a given integer right by the specified
- * number of bits,  as if the operation were being performed within a register
- * that is 12 bits wide.
- *
- * If ''by'' is negative, the bits are instead rotated to the left.
- *
- * The result is undefined if ''bits'' occupies more than just the 12 lowest
- * bits of an integer; that is, if `bits & ~0xFFF` is non-zero.
- *
- * For example:
- * {{{
- *    ror12(0x001, 1) = 0x800
- *    ror12(0x001,25) = 0x800
- *    ror12(0x100, 1) = 0x080
- *    ror12(0x800,-4) = 0x008
- * }}}
- *
- * @param  bits a 12 bit integer
- * @param  by   the number of bit positions to rotate ''bits'' by
- * @return 			the result of rotating ''bits'' right by ''by'' bits
- * @see    [[https://en.wikipedia.org/wiki/Circular_shift]]
- */
+  /**
+   * Rotates the binary representation of a given integer right by a specified
+   * number of bits as if the operation were being performed within a register
+   * exactly 12 bits wide.
+   *
+   * If ''by'' is negative then the bits are instead rotated to the left.
+   *
+   * The result is undefined if ''bits'' occupies more than just the 12 lowest
+   * bits of an integer; that is, if `(bits & ~0xFFF)` is non-zero.
+   *
+   * For example:
+   * {{{
+   *    ror12(0x001, 1) = 0x800
+   *    ror12(0x001,25) = 0x800
+   *    ror12(0x100, 1) = 0x080
+   *    ror12(0x800,-4) = 0x008
+   * }}}
+   *
+   * @param  bits a 12 bit integer
+   * @param  by   the number of bit positions to rotate ''bits'' by
+   * @return 			the result of rotating ''bits'' right by ''by'' bits
+   * @see    [[https://en.wikipedia.org/wiki/Circular_shift]]
+   */
   def ror12(bits: Int,by: ℤ): Int =
   {
     require((bits & ~0xFFF) == 0,"domain error")         // Validate argument
