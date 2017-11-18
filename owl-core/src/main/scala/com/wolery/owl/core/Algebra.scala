6e2f19efdd334c43e3cf99a2ef83f6fb4f792b0b
Jonathon Bell <jonathon.a.bell@gmail.com>
2017-11-18 14:48:47 -0500

    .

--- owl-core/src/main/scala/com/wolery/owl/core/Algebra.scala
+++ owl-core/src/main/scala/com/wolery/owl/core/Algebra.scala
@@ -34,28 +34,31 @@ package core
  *            e ⋅ m = m = m ⋅ e                          identity
  *     (m₁ ⋅ m₂) ⋅ m₃ = m₁ ⋅ (m₂ ⋅ m₃)                   associativity
  * }}}
- * for all `m` in `M`, where ⋅ denotes the function `operate`. In other words,
- * `M` is a semigroup with an identity element.
+ * for all `m` in `M`, where `⋅` denotes the function `operate`.
+ *
+ * In other words, `M` is a semigroup with an identity element.
  *
  * @tparam M  The underlying set on which the monoid operation acts.
  *
  * @see    [[https://en.wikipedia.org/wiki/Monoid Monoid (Wikipedia)]]
+ * @see    [[MonoidSyntax]]
  * @author Jonathon Bell
  */
 trait Monoid[M]
 {
   /**
-   * The identity element; that is, the unique element `e` in `M` such that
-   * `e ⋅ m = m = m ⋅ e` for all `m` in `M`.
+   * The identity element of the monoid `M`;  that is,  the unique element `e`
+   * of `M` such that `e ⋅ m = m = m ⋅ e` for all `m` in `M`, where ⋅  denotes
+   * the function `operate`.
    */
   val e: M
 
   /**
-   * Returns the product of the given elements, whatever this may mean for the
-   * specific algebraic structure in question.
+   * Returns the product of the given pair of elements, whatever this may mean
+   * for the specific algebraic structure in question.
    *
-   * The function is associative; that is, `(m₁ ⋅ m₂) ⋅ m₃ = m₁ ⋅ (m₂ ⋅ m₃)`
-   * for all `m` in `M`.
+   * The function is ''associative''; that is, `(m₁ ⋅ m₂) ⋅ m₃ = m₁ ⋅ (m₂ ⋅ m₃)`
+   * for all `m` in `M`, where `⋅` denotes the function `operate`.
    */
   def operate(m: M,n: M): M
 }
@@ -70,20 +73,23 @@ trait Monoid[M]
  *     (g₁ ⋅ g₂) ⋅ g₃ = g₁ ⋅ (g₂ ⋅ g₃)                   associativity
  *           g ⋅ -g = e = -g ⋅ g                         invertability
  * }}}
- * for all `g` in `G`, where ⋅ and - denote the functions `operate` and `inverse`
- * respectively. In other words, `G` is a monoid in which every element has an
- * inverse.
+ * for all `g` in `G`,  where `⋅` and  `-` denote the  functions `operate` and
+ * `inverse` respectively.
+ *
+ * In other words, `G` is a monoid in which every element has an inverse.
  *
  * @tparam G  The underlying set on which the group operation acts.
  *
  * @see    [[http://en.wikipedia.org/wiki/Group_(mathematics) Group (Wikipedia)]]
+ * @see    [[GroupSyntax]]
  * @author Jonathon Bell
  */
 trait Group[G] extends Monoid[G]
 {
   /**
-   * Returns the inverse of the element `g`; that is, the unique element `-g`
-   * in `G` such that `g + -g = e = -g + g`.
+   * Returns the inverse of the element `g`;  that is, the unique element `-g`
+   * of `G` such  that `g ⋅ -g = e = -g ⋅ g`,  where `⋅` denotes  the function
+   * `operate`.
    */
   def inverse(g: G): G
 }
@@ -93,67 +99,85 @@ trait Group[G] extends Monoid[G]
  *
  * Instances satisfy the axioms:
  * {{{
- *            s ⋅ e = s                                  identity
- *    s ⋅ (g₁ ⋅ g₂) = (s ⋅ g₁) + g₂                      compatability
+ *            s + e = s                                  identity
+ *    s + (g₁ ⋅ g₂) = (s + g₁) + g₂                      compatability
  * }}}
- * for all `s` in `S` and `g` in `G`, where ⋅ denotes the members
- * `zero` and `plus` respectively.
+ * for all `s` in `S` and `g` in `G`,  where `⋅` and  `+` denote the functions
+ * `operate` and `apply` respectively.
  *
- * @tparam G  A group that acts upon the carrier set `S` via the mapping `apply`.
+ * In other words, `+` is a homomorphism from `G` into `Sym(S)`, the symmetric
+ * group consisting of all permutations of `S`,  regarded as a group under the
+ * operation of function composition.
+ *
+ * Group actions,  especially those of ℤ, the integers regarded as an additive
+ * group, are of special interest to us in Owl because they enable us to model
+ * permutations of pitches, notes, scales, and the like,  using simple integer
+ * arithmetic.
+ *
+ * @tparam G  A group that acts upon the carrier set  `S` via the mapping `apply`.
  * @tparam S  A non-empty set acted upon by the group `G` via the mapping `apply`.
  *
  * @see    [[http://en.wikipedia.org/wiki/Group_action Group action (Wikipedia)]]
+ * @see    [[ActionSyntax]]
+ * @author Jonathon Bell
  */
 abstract class Action[S,G](implicit val group: Group[G])
 {
   /**
-   * Applies an element of the group to an element of the carrier set, whatever
-   * this might mean for the actual group action in question.
+   * Applies an element of the group `G` to an element of the carrier set `S`,
+   * whatever this may mean for the specific algebraic structure in question.
    *
-   * Notice that the map `(_ + g)` is necessarily a permutation of the carrier
-   * set `S`, and thus `apply` is a homomorphism from `G` into Aut(`S`),
-   * the set of permutations of `S` regarded as a group under composition of
-   * mappings.
+   * Note that the mapping `_ + g` is necessarily a permutation of the carrier
+   * set `S`, and thus that `apply` is a  homomorphism from `G` into `Sym(S)`,
+   * the symmetric group consisting of all of permutations of `S`, regarded as
+   * a group under the operation of function composition.
    */
   def apply(s: S,g: G): S
 }
 
-/**TODO
+/**
  * Describes a regular (right) action of the group `G` upon the carrier set
  * `S`.
  *
- * Here the term `regular` means that the action is `sharply transitive`;
- * that is, for every pair of elements `s₁` and `s₂` in `S` there exists
- * a unique element `s₂ - s₁` in `G` such that `s₁` + (`s₂` - `s₁`) =
- * `s₂`, where + and - denote the members `apply` and `delta` respectively.
+ * By ''regular'' we mean that the action is ''sharply transitive'';  that is,
+ * for every pair of elements  `s₁` and `s₂` in `S`  there exists a ``unique``
+ * element `s₂ - s₁` in `G` such that `s₁ + (s₂ - s₁) = s₂`, where `+` and `-`
+ * denote the functions `apply` and `delta` respectively.
  *
- * Thus in addition to the axioms for a group action, instances also satisfy
+ * Thus,  in addition to the axioms for a group action, instances also satisfy
  * the axiom:
  * {{{
  *    s₁ + (s₂ - s₁) = s₂                                regularity
  * }}}
- * for all `s` in `S`, where + and - denote the members `apply` and `delta`
- * respectively.
+ * for all `s` in `S`, where `+` and `-` denote the functions `apply` and
+ * `delta` respectively.
  *
- * We say that `S` is a `torsor` for the group `G`, or simply that `S`
- * is a `G-torsor`.
+ * We say that `S` is a ''torsor'' for the group `G`,  or simply that `S` is a
+ * ''G-torsor''.
  *
- * @tparam G  A group that acts regularly upon the carrier set `S` via the mapping `apply`.
+ * Torsors, especially those of ℤ, the integers regarded as an additive group,
+ * and ℤ/''n''ℤ, the ring of integers modulo ''n'', are of special interest to
+ * us in Owl because they they make precise the musical notion of ''interval''
+ * between notes, pitches, frequencies, and so on.
+ *
+ * @tparam G  A group that acts regularly upon the carrier set  `S` via the mapping `apply`.
  * @tparam S  A non-empty set acted upon regularly by the group `G` via the mapping `apply`.
  *
  * @see    [[http://en.wikipedia.org/wiki/Principal_homogeneous_space Torsor (Wikipedia)]]
  * @see    [[http://math.ucr.edu/home/baez/torsors.html Torsors Made Easy (John Baez)]]
+ * @see    [[TorsorSyntax]]
+ * @author Jonathon Bell
  */
 trait Torsor[S,G] extends Action[S,G]
 {
-  /**TODO
-   * Returns the 'delta' between any pair of elements of the carrier set; that
-   * is,  the unique group element that when applied to the first element maps
-   * it into the second.
+  /**
+   * Returns the 'delta' between the given pair of elements of the carrier set
+   * `S`; that is, the unique element `g` in `G` such that `s + g = t`,  where
+   * `+`, denotes the function `apply`.
    *
    * @param  s An element of the carrier set `S`.
    * @param  t An element of the carrier set `S`.
-   * @return The unique group element that maps `s` to `t`.
+   * @return The unique element of `G` that maps `s` into `t`.
    */
   def delta(s: S,t: S): G
 }
--- owl-core/src/main/scala/com/wolery/owl/core/Package.scala
+++ owl-core/src/main/scala/com/wolery/owl/core/Package.scala
@@ -35,31 +35,31 @@ package object core
 
 //****************************************************************************
 
-  implicit final class MonoidEx[M](m: M)(implicit α: Monoid[M])
+  implicit final class MonoidSyntax[M](m: M)(implicit α: Monoid[M])
   {
     def ⋅ (n: M): M                   = α.operate(m,n)
     def + (n: M): M                   = α.operate(m,n)
     def unary_+ : M                   = m
   }
 
-  implicit final class GroupEx[G](f: G)(implicit α: Group[G])
+  implicit final class GroupSyntax[G](f: G)(implicit α: Group[G])
   {
     def - (g: G): G                   = α.operate(f,α.inverse(g))
     def unary_- : G                   = α.inverse(f)
   }
 
-  implicit final class ActionEx[S,G](s: S)(implicit α: Action[S,G])
+  implicit final class ActionSyntax[S,G](s: S)(implicit α: Action[S,G])
   {
     def + (g: G): S                   = α(s,g)
     def - (g: G): S                   = α(s,α.group.inverse(g))
   }
 
-  implicit final class TorsorEx[S,G](s: S)(implicit α: Torsor[S,G])
+  implicit final class TorsorSyntax[S,G](s: S)(implicit α: Torsor[S,G])
   {
     def - (t: S): G                   = α.delta(t,s)
   }
 
-  implicit final class ℤActionEx[S](s: S)(implicit α: Action[S,ℤ])
+  implicit final class ℤActionSyntax[S](s: S)(implicit α: Action[S,ℤ])
   {
     def ♭                             = α(s,-1)
     def ♮                             = s
