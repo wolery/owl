da25822649f130d7a175068a09ab577988aa8343
Jonathon Bell <jonathon.a.bell@gmail.com>
2017-11-24 13:15:14 -0500

    .

--- owl-core/src/test/scala/com/wolery/owl/OwlTest.scala
+++ owl-core/src/test/scala/com/wolery/owl/OwlTest.scala
@@ -18,47 +18,38 @@ package com.wolery.owl
 
 class OwlTest extends OwlSuite
 {
-  test("iff")
+  test("BoolSyntax")
   {
-    assert(true  iff true,            "[1]")
-    reject(true  iff false,           "[2]")
-    reject(false iff true,            "[3]")
-    assert(false iff false,           "[4]")
+    assert(true  iff true,            "[iff 1]")
+    reject(true  iff false,           "[iff 2]")
+    reject(false iff true,            "[iff 3]")
+    assert(false iff false,           "[iff 4]")
+
+    assert(true  implies true,        "[implies 1]")
+    reject(true  implies false,       "implies [2]")
+    assert(false implies true,        "[implies 3]")
+    assert(false implies false,       "[implies 4]")
   }
 
-  test("implies")
+  test("OrderingSyntax")
   {
-    assert(true  implies true,        "[1]")
-    reject(true  implies false,       "[2]")
-    assert(false implies true,        "[3]")
-    assert(false implies false,       "[4]")
+    assert(0  .max(1)   == 1,         "[max 1]")
+    assert(0.0.max(1.0) == 1.0,       "[max 2]")
+    assert('a'.max('b') == 'b',       "[max 3]")
+
+    assert(0  .min(1)   == 0,         "[min 1]")
+    assert(0.0.min(1.0) == 0.0,       "[min 2]")
+    assert('a'.min('b') == 'a',       "[min 3]")
+
+    assert(0.isBetween(0,1),          "[isBetween 1]")
+    assert(1.isBetween(0,1),          "[isBetween 2]")
+    reject(2.isBetween(0,1),          "[isBetween 3]")
+    assert(0.0.isBetween(0.0,1.0),    "[isBetween 4]")
+    assert(1.0.isBetween(0.0,1.0),    "[isBetween 5]")
+    reject(2.0.isBetween(0.0,1.0),    "[isBetween 6]")
   }
 
-  test("min")
-  {
-    assert(0  .min(1)   == 0,         "[1]")
-    assert(0.0.min(1.0) == 0.0,       "[2]")
-    assert('a'.min('b') == 'a',       "[3]")
-  }
-
-  test("max")
-  {
-    assert(0  .max(1)   == 1,         "[1]")
-    assert(0.0.max(1.0) == 1.0,       "[2]")
-    assert('a'.max('b') == 'b',       "[3]")
-  }
-
-  test("isBetween")
-  {
-    assert(0.isBetween(0,1),          "[1]")
-    assert(1.isBetween(0,1),          "[2]")
-    reject(2.isBetween(0,1),          "[3]")
-    assert(0.0.isBetween(0.0,1.0),    "[1]")
-    assert(1.0.isBetween(0.0,1.0),    "[2]")
-    reject(2.0.isBetween(0.0,1.0),    "[3]")
-  }
-
-  test("Seq Operators")
+  test("SeqSyntax")
   {
     val s = Seq(1,2,3)
     val t = Seq(  2,3,4)
@@ -86,7 +77,7 @@ class OwlTest extends OwlSuite
     assert(s ⊉ t,                     "[⊉]")
   }
 
-  test("Set Operators")
+  test("SetSyntax")
   {
     val s = Set(1,2,3)
     val t = Set(  2,3,4)
@@ -114,7 +105,7 @@ class OwlTest extends OwlSuite
     assert(s ⊉ t,                     "[⊉]")
   }
 
-  test("Element Operators")
+  test("ElementSyntax")
   {
     assert(1 ∈ Seq(1,2),              "[∈]")
     assert(1 ∈ Set(1,2),              "[∈]")
