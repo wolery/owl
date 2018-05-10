8966e02f823e841b92893c9701c0ae2ab24554b9
Jonathon Bell <jonathon.a.bell@gmail.com>
2018-05-10 08:21:36 -0400

    Piecewise functions.

--- owl-core/src/main/scala/com/wolery/math/Piecewise.scala
+++ owl-core/src/main/scala/com/wolery/math/Piecewise.scala
@@ -4,7 +4,7 @@
 //*  Version : Header:
 //*
 //*
-//*  Purpose : https://en.wikipedia.org/wiki/Piecewise.
+//*  Purpose :
 //*
 //*
 //*  Comments: This file uses a tab size of 2 spaces.
@@ -19,38 +19,114 @@ import scala.collection.Searching._
 import scala.reflect.ClassTag
 
 /**
+ * Provides support for constructing functions whose definitions are specified
+ * piecewise.
+ *
+ * By `piecewise`
+ * {{{
+ *
+ * }}}
+ *
+ * @see    [[https://en.wikipedia.org/wiki/Piecewise. Piecewise (Wikipedia)]]
+ *
  * @author Jonathon Bell
  */
-object Piecewise
+trait piecewise
 {
   /**
+   * Returns the function whise
    *
+   * @tparam α       The domain of the function to construct.
+   * @tparam β       The range  of the function to construct.
+   *
+   * @param  first   The first `piece` of the function to construct.
+   *
+   *                 Computes the value of the function for all arguments less
+   *                 than `pieces.head._1`.
+   *
+   * @param  pieces  The 'pieces' of the function to construct, in the form of
+   *                 a sequence of value-function pairs `(x,f)`,  where `x` is
+   *                 the greatest lower bound of the sub-domain for which `f`
+   *                 is defined.
+   *
+   * @return         The
+   *
+   * @see    [[https://en.wikipedia.org/wiki/Piecewise Piecewise (Wikipedia]]
    */
-  def apply[α: Ordering: ClassTag,β](min: α ⇒ β,pieces: (α,α ⇒ β)*): Function[α,β] = new Function[α,β]
+  final
+  def piecewise[α: Ordering: ClassTag,β](first: α ⇒ β,pieces: (α,α ⇒ β)*): Function[α,β] = new Function[α,β]
   {
-    val (v,p) =
-    {
-      val (v,p) = pieces.unzip
+    val f:α ⇒ α ⇒ β = step(first,pieces: _*)
 
-      (v.toArray,(min +: p).toArray)
+    def apply(a: α): β = f(a)(a)
+  }
+
+  /**
+   * @see [[https://en.wikipedia.org/wiki/Step_function Step function (Wikipedia]]
+   */
+  final
+  def step[α: Ordering: ClassTag,β: ClassTag](first: β,pieces: (α,β)*): Function[α,β] = new Function[α,β]
+  {
+    val (x: Array[α],f: Array[β]) =
+    {
+      val (x,f) = pieces.unzip
+
+      (x.toArray,(first +: f).toArray)
     }
 
-    def apply(a: α): β = v.search(a) match
+    def apply(a: α): β = x.search(a) match
     {
-      case Found(i)          ⇒ p(i+1)(a)
-      case InsertionPoint(i) ⇒ p(i)  (a)
+      case Found(i)          ⇒ f(i+1)
+      case InsertionPoint(i) ⇒ f(i)
     }
   }
 
   /**
+   * @param  lo
+   * @param  hi
+   * @param  y
+   * @param  x
    *
+   * @return
+   *
+   * @see [[https://en.wikipedia.org/wiki/Boxcar_function Boxcar function (Wikipedia]]
    */
-  def apply[α: Ordering: ClassTag,β](max: β,pieces: (α,β)*): Function[α,β] =
+  final
+  def boxcar(lo: ℝ,hi: ℝ,y: ℝ)(x: ℝ): ℝ =
   {
-    val pairs = for ((a,b) ← pieces) yield
-                     (a, (_: α) ⇒ b)
-    apply((_: α) ⇒ max,pairs: _*)
+    if (x.isBetween(lo,hi)) y else 0
+  }
+
+  /**
+   * @param  x
+   *
+   * @return
+   *
+   * @see [[https://en.wikipedia.org/wiki/Heavisde_function Heaviside function (Wikipedia]]
+   */
+  final
+  def heaviside(x: ℝ): ℤ =
+  {
+    if (x < 0) 0 else 1
+  }
+
+  /**
+   * @param  x
+   *
+   * @return
+   *
+   * @see [[https://en.wikipedia.org/wiki/Sign_function Sign function (Wikipedia]]
+   */
+  final
+  def sign(x: ℝ): ℤ =
+  {
+    if (x < 0) -1 else
+    if (x > 0)  1 else 0
   }
 }
+
+//****************************************************************************
+
+object piecewise extends piecewise
 
 //****************************************************************************
--- owl-core/src/main/scala/com/wolery/owl/temporal/Tempo.scala
+++ owl-core/src/main/scala/com/wolery/owl/temporal/Tempo.scala
@@ -16,7 +16,7 @@ package com.wolery
 package owl
 package temporal
 
-import com.wolery.math.Piecewise
+import com.wolery.math.piecewise.step
 
 //****************************************************************************
 
@@ -30,48 +30,48 @@ case class Tempo(bpm: ℝ) extends AnyVal
 object Tempo
 {
   private
-  val rates: ℝ ⇒ String =
-    Piecewise("Larghissimo",
-       20.0 → "Grave",
-       41.0 → "Lento",
-       46.0 → "Largo",
-       51.0 → "Larghetto",
-       56.0 → "Adagio",
-       65.0 → "Adagietto",
-       70.0 → "Andante moderato",
-       73.0 → "Andante",
-       78.0 → "Andantino",
-       84.0 → "Marcia moderato",
-       86.0 → "Moderato",
-       98.0 → "Allegretto",
-      110.0 → "Allegro",
-      133.0 → "Vivace",
-      141.0 → "Vivacissimo",
-      151.0 → "Allegrissimo",
-      168.0 → "Presto",
-      178.0 → "Prestissimo")
+  val rates: ℝ ⇒ String = step(
+            "Larghissimo",
+     20.0 → "Grave",
+     41.0 → "Lento",
+     46.0 → "Largo",
+     51.0 → "Larghetto",
+     56.0 → "Adagio",
+     65.0 → "Adagietto",
+     70.0 → "Andante moderato",
+     73.0 → "Andante",
+     78.0 → "Andantino",
+     84.0 → "Marcia moderato",
+     86.0 → "Moderato",
+     98.0 → "Allegretto",
+    110.0 → "Allegro",
+    133.0 → "Vivace",
+    141.0 → "Vivacissimo",
+    151.0 → "Allegrissimo",
+    168.0 → "Presto",
+    178.0 → "Prestissimo")
 
-   private
-   val names: Map[String,ℝ] =
-     Map("larghissimo"      →  15.0,
-         "grave"            →  30.5,
-         "lento"            →  43.5,
-         "largo"            →  48.5,
-         "larghetto"        →  53.5,
-         "adagio"           →  60.5,
-         "adagietto"        →  67.5,
-         "andante moderato" →  71.5,
-         "andante"          →  75.5,
-         "andantino"        →  81.0,
-         "marcia moderato"  →  85.0,
-         "moderato"         →  90.0,
-         "allegretto"       → 104.0,
-         "allegro"          → 121.5,
-         "vivace"           → 137.0,
-         "vivacissimo"      → 146.0,
-         "allegrissimo"     → 159.5,
-         "presto"           → 173.0,
-         "prestissimo"      → 178.0)
+  private
+  val names: Map[String,ℝ] = Map(
+    "larghissimo"      →  15.0,
+    "grave"            →  30.5,
+    "lento"            →  43.5,
+    "largo"            →  48.5,
+    "larghetto"        →  53.5,
+    "adagio"           →  60.5,
+    "adagietto"        →  67.5,
+    "andante moderato" →  71.5,
+    "andante"          →  75.5,
+    "andantino"        →  81.0,
+    "marcia moderato"  →  85.0,
+    "moderato"         →  90.0,
+    "allegretto"       → 104.0,
+    "allegro"          → 121.5,
+    "vivace"           → 137.0,
+    "vivacissimo"      → 146.0,
+    "allegrissimo"     → 159.5,
+    "presto"           → 173.0,
+    "prestissimo"      → 178.0)
 
   def apply(name: String): Option[Tempo] =
   {
